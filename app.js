abd339bfbb0bd57d9b8db13d1a1b2bbf1b0781da
Bashar-Owainat <owainatbashar13@gmail.com>
2021-08-01 22:00:44 +0300

    added html and css and js code

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+let userName = prompt("What is your name? ")
+document.write(`<h2> ${userName}</h2>`)
+
+let userMajor = prompt("what do you study? ")
+alert("Nice!");
+
+let age = prompt("Tell us your age")
+
+let message = prompt("did you like my website? ")
